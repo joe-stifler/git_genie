e4d283f1a96514b6e809fade8761aa724ebb7d28
joe-stifler <joseribeiro1017@gmail.com>
2024-03-27 12:52:22 +0000

    feat: add GitRepo, CLIHandler, MarkdownGenerator, and main to __init__.py

---
+++ git_genie/__init__.py
@@ -0,0 +1,6 @@
+from .git_repo import GitRepo
+from .cli_handler import CLIHandler
+from .markdown_generator import MarkdownGenerator
+from .main import main
+
+__all__ = ['GitRepo', 'CLIHandler', 'MarkdownGenerator', 'main']
